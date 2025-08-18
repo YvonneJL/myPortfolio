6a65bd475f36cf2e8947796af3862bd7b3a8747f
Yvi <blabla@Air-von-Yvi.fritz.box>
2025-08-18 13:04:27 +0200

    converted vids from webm to mp4

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,28 +6,28 @@ import SingleProject from "./SingleProject";
 export default function Projects() {
 
     const projects: IProject[] = [{
-        imgSrc: "/video/iPhone-13-PRO-hangman-game-seven-xi.vercel.app-5x_k3dnhewk_ug.webm",
+        imgSrc: "/video/iPhone-13-PRO-hangman-game-seven-xi.vercel.app-5x_k3dnhewk_ug.mp4",
         name: "Hangman Project",
         github: "https://github.com/YvonneJL/Hangman-Game",
         deployment: "https://hangman-game-seven-xi.vercel.app/",
         needsRotate: true,
     },
     {
-        imgSrc: "/video/iPhone-13-PRO-rock-paper-scissors-react-project.vercel.app-bsur93f9j-mdjw.webm",
+        imgSrc: "../../public/video/iPhone-13-PRO-rock-paper-scissors-react-project.vercel.app-bsur93f9j-mdjw.mp4",
         name: "Rock Paper Scissors Project",
         github: "https://github.com/YvonneJL/Rock-Paper-Scissors-React-Project?tab=readme-ov-file",
         deployment: "https://rock-paper-scissors-react-project.vercel.app/",
         needsRotate: false
     },
     {
-        imgSrc: "/video/iPhone-13-PRO-pokemon-api-duo-project.vercel.app--xd977wtq3_bby.webm",
+        imgSrc: "/video/iPhone-13-PRO-pokemon-api-duo-project.vercel.app--xd977wtq3_bby.mp4",
         name: "Pokemon Team Project",
         github: "https://github.com/YvonneJL/Project-Pokemon-API",
         deployment: "https://pokemon-api-duo-project.vercel.app/pikachu",
         needsRotate: false,
     },
     {
-        imgSrc: "/video/iPhone-13-PRO-mov-react-project.vercel.app-n8u03m2wv1q__8.webm",
+        imgSrc: "/video/iPhone-13-PRO-mov-react-project.vercel.app-n8u03m2wv1q__8.mp4",
         name: ".MOV Project",
         github: "https://github.com/YvonneJL/.MOV-React-Project",
         deployment: "https://mov-react-project.vercel.app/",
